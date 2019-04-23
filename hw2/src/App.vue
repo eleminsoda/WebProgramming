e5dc586c94cee12977949308110d5af146672366
lord-maul <987420788yh@gmail.com>
2019-04-23 23:39:10 +0800

    start hw2 using @vue/cli

---
+++ hw2/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <film-brief v-for="film in films" v-bind:key="film._id" v-bind:film="film"></film-brief>
+  </div>
+</template>
+
+<script>
+import FilmBrief from "./components/FilmBrief.vue";
+import films from "./assets/films.json";
+
+export default {
+  name: "app",
+  data: function() {
+    return {
+      films
+    }
+  },
+  components: {
+    FilmBrief
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ hw2/src/components/FilmBrief.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="film-brief">
+    <div class="poster">
+      <img :src="film.poster">
+    </div>
+
+    <div class="info">
+      <div class="heading">{{film.title}}</div>
+      <div class="ratings">{{film.rating.average}}</div>
+      <div class="ontime">{{film.pubdate}}</div>
+      <div class="type">{{film.genres}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FilmBrief",
+  props: ['film']
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
